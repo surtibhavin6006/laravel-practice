8d291bfe326c6b30a23af18db358ae3c16162ff5
surtibhavin6006 <surtibhavin6006@gmail.com>
2022-09-20 09:28:37 +0530

    remove debug code

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -20,7 +20,6 @@ class EventController extends Controller
 
     public function all(Request $request)
     {
-        dd(Carbon::now()->month);
         // get all list
         $events = $this->eventRepository->getAllEvents(null);
 
